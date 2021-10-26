c536e1ef61669ac512e382d30bc6e643658671f5
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-10-27 00:39:47 +0300

    edit cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,17 +5,21 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
-// eslint-disable-next-line consistent-return
-module.exports = ((req, res, next) => {
-  const { origin } = req.headers;
+module.exports = (req, res, next) => {
+  const { origin } = req.headers
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-  const { method } = req;
-  if (method === 'OPTIONS' || allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.status(200).send({ message: 'OK' });
   }
+
   next();
-});
+};
