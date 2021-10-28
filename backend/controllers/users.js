2cfb27ca1707c0a8487156fbda07b34df4356490
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-10-28 22:02:41 +0300

    fixed

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,4 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -67,7 +68,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       res.send({
-        token: jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' }),
+        token: jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret', { expiresIn: '7d' }),
       });
     })
     .catch(() => {
