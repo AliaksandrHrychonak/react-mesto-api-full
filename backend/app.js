0f5020909237da18554a9e866c8e10e560cba573
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2021-10-28 19:44:27 +0300

    pull .env

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const mongoose = require('mongoose', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-app.use('*', cors)
+app.use(cors)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
